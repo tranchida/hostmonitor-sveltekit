fa86b2f730f4380728f5141934c77eeb758870d6
Giampaolo Tranchida <giampaolo@tranchida.ch>
2026-03-01 12:28:33 +0100

    fix: switch to adapter-auto and add vitePreprocess for SvelteKit configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consultez https://svelte.dev
+	// pour en savoir plus sur les préprocesseurs
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter({
-			out: 'build'
-		})
+		// adapter-auto ne supporte que certains environnements, voir 
+		// https://svelte.dev pour une liste.
+		// Si votre environnement n'est pas supporté, ou si vous avez choisi 
+		// un environnement spécifique, changez d'adaptateur.
+		// Voir https://svelte.dev/docs/kit/adapters pour plus d'infos.
+		adapter: adapter()
 	}
 };
 
